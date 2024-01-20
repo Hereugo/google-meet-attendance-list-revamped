be8ec485fa1f7a01654cff0b1c2116cc283d276f
Амир Нурмухамбетов <thereug6@gmail.com>
2024-01-20 01:39:55 +0100

    - Styling modal; - Integrated with Google meet;

--- js/components/ListView.js
+++ js/components/ListView.js
@@ -7,36 +7,68 @@ class ListView extends Modal {
         document.body.insertAdjacentHTML('beforebegin', `
         <template id="gmal__list">
             <div id="modal__background">
-                <div id="popup">
-                    <button id="popup__close"><svg-icon name="close-big"></svg-icon></button>
+                <div id="popup" class="gmal__list_popup">
+                    <div id="popup__header">
+                        <h2 id="popup__title">Available Classes</h2>
+                        <button id="popup__close">
+                            <i class="fal fa-lg fa-times"></i>
+                        </button>
+                    </div>
                     <div id="popup__content">
-                        <p>List view</p>
+                        <div class="gmal__list_search">
+                            <input type="text" placeholder="Search for a class" />
+                        </div>
+                        <ul class="gmal__list_content">
+                        </ul>
                     </div>
                 </div>
             </div>
         </template>
-        `);   
+        `);
     }
 
     open() {
         super.open();
 
         // TODO Fill in the popup content
-        // document.getElementById("popup__content");
-        // chrome.storage.sync.get(["classes"]).then((result) => {
-        //     let participants = JSON.parse(sessionStorage.getItem("participants"));
-        //     let classes = result.classes;
+        chrome.storage.sync.get(["classes"]).then((result) => {
+            let participants = JSON.parse(sessionStorage.getItem("participants"));
+            let classes = result.classes;
             
-        //     let html = ``;
+            let listItem = `
+            <li class="gmal__list_item">
+                <gmal-detail>
+                    <span class="gmal__list_item_title">Physics</span>
+                    <div class="gmal__list_item_details">
+                        <div class="gmal__list_item_profiles">
+                            <img src="https://picsum.photos/32/32" alt="random image" />
+                            <img src="https://picsum.photos/32/32" alt="random image" />
+                            <div class="gmal__list_item_profile_more">
+                                <span>+3</span>
+                            </div>
+                        </div>
+                        <span class="footnote">5 out of 10 present</span>
+                    </div>
+                    <div class="gmal__list_item_actions">
+                        <button class="gmal__list_item_action">
+                            <i class="far fa-edit"></i>
+                        </button>
+                        <button class="gmal__list_item_action">
+                            <i class="far fa-trash"></i>
+                        </button>
+                    </div>
+                </gmal-detail>
+            </li>
+            `;
 
-        //     // modify html and append to popup__content
-        //     document.getElementById("popup__content").insertAdjacentHTML("beforeend", html);
-        // });
-        // console.log("Open list view: ", participants);
+            // modify html and append to popup__content
+            document.getElementById("gmal__list_content").insertAdjacentHTML("beforeend", html);
+        });
+        console.log("Open list view: ", participants);
     }
 
     connectedCallback() {
-        // this.injectTemplate();
+        this.injectTemplate();
 
         const btn = Utils.createElement("button", null, null, "List View");
         this.appendChild(btn);
